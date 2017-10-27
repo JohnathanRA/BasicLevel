01db517e89a7e796805bbfc55b7de52e3093244d
JohnathanRA <31198069+JohnathanRA@users.noreply.github.com>
2017-10-26 23:24:33 -0500

    Update Battleship.c
    Game of Naval Battle.

--- Battleship.c
+++ Battleship.c
@@ -29,7 +29,6 @@ main(){
 		i=(rand()%10)+0;
     	j=(rand()%10)+0;
     	if(PC[i][j]=='_'){
-			T2[i][j]='o';
 			PC[i][j]='o';
 			a++;
     	}
@@ -39,25 +38,29 @@ main(){
 		i=-1, j=-1;
 		printf("Da coordenadas para colocar tu barco #%d: ", a+1);
 		scanf("%d %d", & i, & j);
-		if(T1[i][j]=='_'){
-			T1[i][j]='o';
-			a++;
-			printf("\t\t J1 \t\t\t\t PC\n");
-			printf("   0  1  2  3  4  5  6  7  8  9\t    0  1  2  3  4  5  6  7  8  9\n");
-			for(i=0;i<10;i++){
-				printf("%d", i);
-				for(j=0;j<10;j++){
-					printf("  %c", T1[i][j]);
+		if(i<=9 && j<=9){
+			if(T1[i][j]=='_'){
+				T1[i][j]='o';
+				a++;
+				printf("\t\t J1 \t\t\t\t PC\n");
+				printf("   0  1  2  3  4  5  6  7  8  9\t    0  1  2  3  4  5  6  7  8  9\n");
+				for(i=0;i<10;i++){
+					printf("%d", i);
+					for(j=0;j<10;j++){
+						printf("  %c", T1[i][j]);
+					}
+					printf("\t %d", i);
+					for(j=0;j<10;j++){
+						printf("  %c", T2[i][j]);
+					}
+					printf("\n");
 				}
-				printf("\t %d", i);
-				for(j=0;j<10;j++){
-					printf("  %c", T2[i][j]);
-				}
-				printf("\n");
 			}
-		}
-		if(T1[i][j]=='o'){
-			printf("Ya hay un barco en las coordenadas: %d %d\n", i, j);
+			if(T1[i][j]=='o'){
+				printf("Ya hay un barco en las coordenadas: %d %d.\n", i, j);
+			}
+		}else{
+			printf("Las coorenadas deben estar dentro del rango [0,9].\n");	
 		}
 		if(i==-1 || j==-1){
 			printf("CARACTERES NO CARNAL!\nYa lo arruinaste.");
@@ -72,40 +75,44 @@ main(){
 			do{
 				k=(rand()%10)-1;
     			h=(rand()%10)-1;
-    			if(T1[k][h]=='_'){
+    			if(k<=9 && h<=9){
+	    			if(T1[k][h]=='_'){
+						b=1;
+					}
+					if(T1[k][h]=='o'){
+						b=2;
+						c++;
+					}
+					if(T1[k][h]=='/'){
+						b=3;
+					}
+					if(T1[k][h]=='x'){
+						b=4;
+					}
+    				switch(b){
+						case 1:
+							T1[k][h]='/';
+							b=0;
+							break;
+						case 2:
+							T1[k][h]='x';
+							printf("TE ESTAN PARTIENDO TU MANDARINA EN GAJOS.\n");
+							b=0;
+							break;
+						case 3:
+							b=1;
+							break;
+						case 4:
+							b=1;
+							break;
+    				}
+				}else{
 					b=1;
 				}
-				if(T1[k][h]=='o'){
-					b=2;
-					c++;
-				}
-				if(T1[k][h]=='Q'){
-					b=3;
-				}
-				if(T1[k][h]=='x'){
-					b=4;
-				}
-    			switch(b){
-					case 1:
-						T1[k][h]='Q';
-						b=0;
-						break;
-					case 2:
-						T1[k][h]='x';
-						printf("Te están partiendo tu mandarina en gajos");
-						b=0;
-						break;
-					case 3:
-						b=1;
-						break;
-					case 4:
-						b=1;
-						break;
-    			}
 			}while(b==1);
 		}
 		if(c==10){
-			printf("YA PERDISTE, NO RIFAS!");
+			printf("YA PERDISTE, NO RIFAS NADA!");
 			break;
 		}
 		if(PC[i][j]=='o'){
@@ -127,12 +134,12 @@ main(){
 			}
 		printf("ESTAS QUE FLIPAS CHAVAL!\n");
 		}
-		if(T2[i][j]=='x' || T2[i][j]=='Q'){
-			printf("Ya has lanzado un misil en las coordenadas %d %d\n", i, j);
+		if(T2[i][j]=='x' || T2[i][j]=='/'){
+			printf("Ya has lanzado un misil en las coordenadas %d %d.\n", i, j);
 		}
 		if(PC[i][j]=='_'){
-			T2[i][j]='Q';
-			PC[i][j]='Q';
+			T2[i][j]='/';
+			PC[i][j]='/';
 			printf("\t\t J1 \t\t\t\t PC\n");
 			printf("   0  1  2  3  4  5  6  7  8  9\t    0  1  2  3  4  5  6  7  8  9\n");
 			for(i=0;i<10;i++){
@@ -146,7 +153,7 @@ main(){
 				}
 				printf("\n");
 			}
-			printf("PERDEDOR! :(\n");
+			printf("PERDEDOR!, FALLASTE!\n");
 		}
 	}
 	printf("YA LE HICISTE CAPIRUCHO! ;)");
